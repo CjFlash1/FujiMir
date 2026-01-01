3581e4cc5cbd297758696b5a67560826f5948602
FujimirDev <dev@fujimir.local>
2026-01-01 01:19:49 +0000

    Configure schema.prisma with extensive binaryTargets and reset deploy.js env

--- deploy.js
+++ deploy.js
@@ -70,7 +70,6 @@ try {
     // 4. DATABASE
     console.log("\n--- DB SETUP ---");
     try {
-        process.env.PRISMA_CLI_BINARY_TARGETS = 'native,debian-openssl-1.1.x,debian-openssl-3.0.x,linux-musl-openssl-3.0.x';
         const prismaBin = path.join(process.cwd(), 'node_modules', '.bin', 'prisma');
         if (fs.existsSync(prismaBin)) {
             run(`${prismaBin} generate`);
